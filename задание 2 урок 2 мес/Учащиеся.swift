ec9275383f5d8a115aba001f256d2bf2f35edd2a
Memka <mkasenova87@gmail.com>
2023-02-06 19:37:15 +0600

    доп

---
+++ задание 2 урок 2 мес/Учащиеся.swift
@@ -0,0 +1,21 @@
+//
+//  Учащиеся.swift
+//  задание 2 урок 2 мес
+//
+//  Created by merim kasenova on 4/2/23.
+//
+
+import Foundation
+
+class Uchashiysya {
+    var marks:Int
+    var timeTable:Bool
+    var uniForm:String
+    
+    init(marks: Int, timeTable: Bool, uniForm: String) {
+        self.marks = marks
+        self.timeTable = timeTable
+        self.uniForm = uniForm
+    }
+    
+}
